a76d8d7810a5e4376ce5215f779b4e51d35b95fc
unknown <lokyung.chan@ontariotechu.net>
2025-11-12 18:23:09 -0500

    added form for generating quiz

--- src/layout/course-layout-views/course-quiz-layout.jsx
+++ src/layout/course-layout-views/course-quiz-layout.jsx
@@ -1,8 +1,10 @@
 import React, {useEffect,useState} from "react";
+import AddButton from "../../assets/AddButton.png";
 
 
 
 export default function QuizView(){
+  const [newQuiz, setNewQuizForm] = useState(false);
     const quizzes = [
     {
       quiz_id: 1,
@@ -44,6 +46,51 @@ export default function QuizView(){
             </p>
           </div>
         ))}
+      </div>
+      <button  className="flex items-center gap-2 px-4 pb-2 mb-4 rounded 
+             bg-transparent text-blue-600 
+             hover:bg-gray-200 
+             focus:outline-none focus:ring-0 
+             active:outline-none active:ring-0 
+             border-none transition"
+  style={{ outline: "none", boxShadow: "none" }}
+          onClick={() => setNewQuizForm(true)}>
+          <img src={AddButton} alt="Make Quiz" className="w-5 h-5" />
+          Generate a New Quiz
+        </button>
+      {newQuiz && <NewQuizForm onClose={() => setNewQuizForm(false)} />}
+    </div>
+  );
+}
+
+function NewQuizForm(){
+  const [numQuestions, setNumQuestions] = useState(5);
+
+  const generateQuiz = (e) => {
+    alert("generate a quiz");
+  }
+  
+  return(
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-white rounded-xl shadow-lg p-6 w-[90%] max-w-md">
+        <form onSubmit={() => generateQuiz}>
+          <h2 className="text-xl font-semibold mb-4 text-gray-800">Make a New Quiz</h2>
+          <p className="text-l font-semibold mb-4 text-black">Reference File:</p>
+          <input type="file" className="w-full text-black bg-white border border-gray-300 rounded p-2 appearance-none file:bg-blue-600 file:text-white file:border-0 file:rounded file:px-3 file:py-1 hover:file:bg-blue-700 mb-4"></input>
+          <p className="text-l font-semibold mb-2 text-black">Number of Question: {numQuestions}</p>
+          <div className="mb-4">
+            <input type="range" min="1" max="10" value={numQuestions} onChange={(e) => setNumQuestions(parseInt(e.target.value))} className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer"
+              style={{
+                background: `linear-gradient(to right, #3b82f6 0%, #3b82f6 ${(numQuestions - 1) * 11.11}%, #e5e7eb ${(numQuestions - 1) * 11.11}%, #e5e7eb 100%)`
+              }}
+            />
+            <div className="flex justify-between text-sm text-gray-600 mt-1">
+              <span>1</span>
+              <span>10</span>
+            </div>
+          </div>
+          <button type="submit" className="w-full bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg hover:bg-blue-700 transition"> Generate </button>
+        </form>
       </div>
     </div>
   );
